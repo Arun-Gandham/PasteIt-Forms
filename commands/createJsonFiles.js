51e3f915c5a3441d7099c8140f14f9bffea3118a
Arun Gandham <arun.g@Technovert.com>
2024-02-21 17:54:00 +0530

    Added folder path and code

--- commands/createJsonFiles.js
+++ commands/createJsonFiles.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Function to generate JSON data
 function generateJSON() {
   const data = {
-    message: 'Hello, world!',
-    timestamp: new Date().toISOString()
+    message: "Hello, world!",
+    timestamp: new Date().toISOString(),
   };
   return JSON.stringify(data, null, 2);
 }
@@ -20,9 +20,24 @@ function writeJSONFile(filePath, jsonData) {
 
 // Main function
 function main() {
-  const rootPath = process.cwd(); // Get current working directory (root path)
+  const rootPath = process.cwd()+ "/PasteIt-Forms"; // Get current working directory (root path)
+
+  if (fs.existsSync(rootPath)) {
+    console.log(rootPath + ' The directory exists.');
+  } else {
+    console.log('The directory does not exist.');
+    fs.mkdir(rootPath, (err) => {
+        if (err) {
+          return console.error(err);
+        }
+        console.log("Directory created successfully!");
+      });
+  }
+
+  
+
   const jsonData = generateJSON(); // Generate JSON data
-  const jsonFilePath = path.join(rootPath+"/PasteIt-Forms", 'output.json'); // Construct file path
+  const jsonFilePath = path.join(rootPath, "output.json"); // Construct file path
   writeJSONFile(jsonFilePath, jsonData); // Write JSON data to file
 }
 
